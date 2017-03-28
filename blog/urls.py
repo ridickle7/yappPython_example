6336e42db862c38e2657cc97654a441870a1bf4e
ridickle <ridickle7@gmail.com>
2017-03-28 19:53:15 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),  #views의 post_list를 호출
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
+
+# ^ : "시작"
+# post/     : post를 포함해야한다
+# (?P<pk>)  : 괄호안에 넣은 모든 것을 변수 pk에 저장
+# [0-9]     : 숫자 0부터 9까지 하나의 숫자만 있다
+# +         : 최소 하나의 숫자 이상이 있어야 한다
